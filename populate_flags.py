33b0ec5266cf5f15f1e412656d689f3596baf762
ericwathome <ericwathome007@gmail.com>
2024-03-29 09:21:14 +0300

    updated flag path

--- populate_flags.py
+++ populate_flags.py
@@ -1,5 +1,4 @@
 import json
-import requests
 
 # Load the JSON file
 with open('currencies.json') as f:
@@ -7,27 +6,10 @@ with open('currencies.json') as f:
 
 # Iterate over the data
 for currency in data:
-    code = currency['code'].lower()  # 'code' is the key for currency code
-
-    png = "https://raw.githubusercontent.com/ericwafula/currency-flags/main/assets/flags_png_rectangle/{code}.png"
-    svg = "https://raw.githubusercontent.com/ericwafula/currency-flags/6cb2908c2077a257e0aac26f13ea884d965c074a/assets/flags_svg/{code}.svg"
-
-    pngResponse = requests.get("https://raw.githubusercontent.com/ericwafula/currency-flags/main/assets/flags_png_rectangle/{code}.png")
-    svgResponse = requests.get("https://raw.githubusercontent.com/ericwafula/currency-flags/6cb2908c2077a257e0aac26f13ea884d965c074a/assets/flags_svg/{code}.svg")
-
-    # Check if the response is successful
-    if pngResponse.status_code == 404:
-        png = None
-    else:
-        png = "https://raw.githubusercontent.com/ericwafula/currency-flags/main/assets/flags_png_rectangle/{code}.png"
-
-    if svgResponse.status_code == 404:
-        svg = None
-    else:
-        svg = "https://raw.githubusercontent.com/ericwafula/currency-flags/6cb2908c2077a257e0aac26f13ea884d965c074a/assets/flags_svg/{code}.svg"
-
-    currency['flag']['png'] = f"https://raw.githubusercontent.com/ericwafula/currency-flags/main/assets/flags_png_rectangle/{code}.png"
-    currency['flag']['svg'] = f"https://raw.githubusercontent.com/ericwafula/currency-flags/6cb2908c2077a257e0aac26f13ea884d965c074a/assets/flags_svg/{code}.svg"
+    # Check if the 'png' key exists in the 'flag' object
+    if 'png' in currency['flag']:
+        # Replace 'flag_png' with 'flags_png' in the URL
+        currency['flag']['png'] = currency['flag']['png'].replace('flag_png', 'flags_png')
 
 # Save the updated data back to the JSON file
 with open('currencies.json', 'w') as f:
